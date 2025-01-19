efe82e7c6f22a1e2f58c069b260cc3f5b58d4c3c
Oleg <37769766+TheDearbear@users.noreply.github.com>
2025-01-19 17:15:48 +0300

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+byte[] bytes = File.ReadAllBytes("input.txt");
+StringBuilder sb = new StringBuilder(bytes.Length / 8);
+
+int bit = 0;
+byte semiResult = 0;
+foreach (byte value in bytes)
+{
+	byte numValue = (byte)(value - 0x30); // '0' == 0x30
+
+	semiResult <<= 1;
+	semiResult += numValue;
+
+	if (++bit == 8)
+	{
+		sb.Append((char)semiResult);
+		bit = 0;
+		semiResult = 0;
+	}
+}
+
+File.WriteAllText("output.txt", sb.ToString());
