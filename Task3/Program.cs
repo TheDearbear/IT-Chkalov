9f0c810acb8ae9d5a8a1a880f8e719380a9e9fe1
Oleg <37769766+TheDearbear@users.noreply.github.com>
2025-01-19 17:50:08 +0300

    Add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+
+string encoded = File.ReadAllText("signal.txt");
+StringBuilder sb = new StringBuilder(encoded.Length);
+
+for (int i = 0; i < encoded.Length; i++)
+{
+	if (!char.IsAsciiLetter(encoded[i]))
+	{
+		sb.Append(encoded[i]);
+		continue;
+	}
+	bool isLower = char.IsAsciiLetterLower(encoded[i]);
+
+	char result = (char)(encoded[i] - 3);
+	if (result < 'A' || (result < 'a' && isLower))
+	{
+		result += (char)26;
+	}
+
+	sb.Append(result);
+}
+
+File.WriteAllText("decoded.txt", sb.ToString());
