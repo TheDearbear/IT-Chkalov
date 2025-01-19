a8a93aa10caa7c68eb1a3afdf163a60439ca3fb9
Oleg <37769766+TheDearbear@users.noreply.github.com>
2025-01-19 17:30:14 +0300

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+string[] lines = File.ReadAllLines("in.txt");
+
+List<double> sin = new List<double>(lines.Length);
+List<double> cos = new List<double>(lines.Length);
+
+foreach (string line in lines)
+{
+	double radians = double.Parse(line) * (Math.PI / 180);
+
+	(double currentSin, double currentCos) = Math.SinCos(radians);
+	
+	sin.Add(currentSin);
+	cos.Add(currentCos);
+}
+
+double result = Math.Atan(sin.Average() / cos.Average()) * (180 / Math.PI);
+
+File.WriteAllText("out.txt", Math.Round(result, 6).ToString());
